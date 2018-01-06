91be0264c9ed22ede443fa9d0f2b44550c728bfe
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-07 06:56:05 +1100

    Implemented a listview to show the tech-stack titles. Data was fetched via redux

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
-  render() {
+  componentWillMount() {
+    // init a datasource for list view
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
 
-    return;
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem library={library} />;
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow} // need to specify how to render a single row
+      />
+    );
   }
 }
 
