f5e2c69a208676814b8d4c9829230a945519a72c
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-12 22:13:43 +1100

    Added an selectLibrary action creator

--- tech_stack/src/reducers/index.js
+++ tech_stack/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 // make multiple reducers work together nicely
 export default combineReducers({
   // key (libraries): value (reducer)
   // application's state will be descirbed by key
-  libraries: LibraryReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
